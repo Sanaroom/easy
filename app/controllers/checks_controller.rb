078c261602e894e85c941cc44b39f406322116a3
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-17 21:22:52 +0900

    numberのコレクション作成

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -3,4 +3,19 @@ class ChecksController < ApplicationController
     @checks=Check.all
   end
 
+  def new
+    @check=Check.new
+  end
+
+  def create
+    @check=Check.new(check_params)
+    
+  end
+
+  private
+  def check_params
+    params.require(:check).permit(:number,:attendance)
+  end
+
+
 end
--- app/models/check.rb
+++ app/models/check.rb
@@ -1,2 +1,11 @@
 class Check < ApplicationRecord
+  with_options presence: true do
+    validates :number , numericality: { other_than: 1 , message: "can't be blank"} 
+    validates :attendance
+  end
+
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :number
 end
